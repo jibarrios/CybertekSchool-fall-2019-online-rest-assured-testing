276cc4df0677b647b9b98e398e174c25bff9f2b1
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-05-17 13:51:29 -0400

    added dependencies

--- src/test/java/com/automation/tests/day2/ORDSTests.java
+++ src/test/java/com/automation/tests/day2/ORDSTests.java
@@ -1,4 +1,21 @@
 package com.automation.tests.day2;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+
 public class ORDSTests {
+    String BASE_URL = "http://3.90.112.152:1000/ords/hr";
+
+    @Test
+    @DisplayName("Get list of all employees")
+    public void getAllEmployees() {
+        Response response = given().
+                                baseUri(BASE_URL).
+                            when().
+                                get("/employees").prettyPeek();
+    }
 }
