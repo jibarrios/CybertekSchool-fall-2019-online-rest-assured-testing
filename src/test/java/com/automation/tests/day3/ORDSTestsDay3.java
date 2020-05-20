8b956bda9a86d6bf7f2a0f56ca7e4615c9f06d34
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-05-19 21:59:39 -0400

    day 3 API

--- src/test/java/com/automation/tests/day3/ORDSTestsDay3.java
+++ src/test/java/com/automation/tests/day3/ORDSTestsDay3.java
@@ -1,11 +1,16 @@
 package com.automation.tests.day3;
+
+import io.restassured.http.ContentType;
 import io.restassured.http.Headers;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,18 +19,59 @@ import static org.hamcrest.Matchers.*;
 public class ORDSTestsDay3 {
 
     @BeforeAll
-    public static void setup(){
+    public static void setup() {
         baseURI = "http://54.224.118.38:1000/ords/hr";
     }
 
     /**
-     * given path parameter is "/regions/{id}"
+     * given resource path is "/regions/{id}"
      * when user makes get request
      * and region id is equals to 1
      * then assert that status code is 200
      * and assert that region name is Europe
+     * and assert that region id is 1
      */
 
+    @Test
+    public void verifyFirstRegion() {
+        given().
+                pathParam("id", 1).
+                when().
+                get("/regions/{id}").prettyPeek().
+                then().assertThat().
+                statusCode(200).
+                body("region_name", is("Europe")).
+                body("region_id", is(1)).
+                time(lessThan(5L), TimeUnit.SECONDS); //verify that response time is less than 5 seconds
+
+    }
+
+    @Test
+    public void verifyEmployee() {
+        Response response = given().
+                accept(ContentType.JSON).
+                when().
+                get("/employees");
+
+        /**
+         * sonPath is an alternative to using XPath for easily getting values from a Object document. It follows the
+         *  * Groovy <a href="http://docs.groovy-lang.org/latest/html/documentation/#_gpath">GPath</a>
+         *  syntax when getting an object from the document. You can regard it as an alternative to XPath for JSON.
+         */
+        JsonPath jsonPath = response.jsonPath();
+
+        //items - name of the array where all employees are stored
+        //GPath, something like XPath bit different. GPath use Groovy syntax
+        String nameOfFirstEmployee = jsonPath.getString("items[0].first_name");//0 - to get first item in the list
+        String nameOfLastEmployee = jsonPath.getString("items[-1].first_name");//-1 - to get last item in the list, like length() - n
+
+        System.out.println("First name of 1st employee: " + nameOfFirstEmployee);
+        System.out.println("First name of last employee: " + nameOfLastEmployee);
+
+
+        Map<String, ?> firstEmployee = jsonPath.get("items[0]");
+        System.out.println(firstEmployee);
+    }
 
 
 }
