9eafe8c7b9e556f3eef16388f6520b4b84ebfb9a
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-05-20 20:03:46 -0400

    day 4 API

--- src/test/java/com/automation/tests/day4/ORDSTestsDay4.java
+++ src/test/java/com/automation/tests/day4/ORDSTestsDay4.java
@@ -44,6 +44,7 @@ public class ORDSTestsDay4 {
         then().
                 assertThat().
                 statusCode(200).
+                header("Content-Type", "application/json").
                 contentType(ContentType.JSON).
                 time(lessThan(3L), TimeUnit.SECONDS);
     }
@@ -54,6 +55,29 @@ public class ORDSTestsDay4 {
          When users sends a GET request to "/countries"
          Then status code is 200
          And Content type is application/json
+         And country_name from payload is "United States of America"
      *
      */
+
+    @Test
+    @DisplayName("Verify country name, content type and status code for country with ID US")
+    public void verifyCountriesTest1(){
+        given().
+                accept(ContentType.JSON).
+                queryParam("q", "{\"country_id\":\"US\"}").
+        when().
+                get("/countries").
+        then().assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).
+                body("items[0].country_name", is("United States of America"));
+
+        ///SECOND REQUEST
+        Response response = given().
+                                accept(ContentType.JSON).
+                            when().
+                                get("/countries").prettyPeek();
+    }
+
+
 }
