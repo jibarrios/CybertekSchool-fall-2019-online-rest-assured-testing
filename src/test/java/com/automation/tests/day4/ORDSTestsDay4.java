f37a1883d3b9944a3cdf0dd2eb08b0ac7660a6d6
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-05-20 20:44:20 -0400

    day 4 API

--- src/test/java/com/automation/tests/day4/ORDSTestsDay4.java
+++ src/test/java/com/automation/tests/day4/ORDSTestsDay4.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -36,47 +37,65 @@ public class ORDSTestsDay4 {
 
     @Test
     @DisplayName("Verify status code, content type and response time")
-    public void employeesTest1(){
+    public void employeesTest1() {
         given().
                 accept(ContentType.JSON).
-        when().
+                when().
                 get("/employees").prettyPeek().
-        then().
+                then().
                 assertThat().
                 statusCode(200).
                 header("Content-Type", "application/json").
                 contentType(ContentType.JSON).
                 time(lessThan(3L), TimeUnit.SECONDS);
     }
+
     /**
-     *
-         Given accept type is JSON
-         And parameters: q = {"country_id":"US"}
-         When users sends a GET request to "/countries"
-         Then status code is 200
-         And Content type is application/json
-         And country_name from payload is "United States of America"
-     *
+     * Given accept type is JSON
+     * And parameters: q = {"country_id":"US"}
+     * When users sends a GET request to "/countries"
+     * Then status code is 200
+     * And Content type is application/json
+     * And country_name from payload is "United States of America"
      */
 
     @Test
     @DisplayName("Verify country name, content type and status code for country with ID US")
-    public void verifyCountriesTest1(){
+    public void verifyCountriesTest1() {
         given().
                 accept(ContentType.JSON).
                 queryParam("q", "{\"country_id\":\"US\"}").
-        when().
+                when().
                 get("/countries").
-        then().assertThat().
+                then().assertThat().
                 statusCode(200).
                 contentType(ContentType.JSON).
                 body("items[0].country_name", is("United States of America"));
 
         ///SECOND REQUEST
+        //accept(ContentType.JSON). - to request JSON from the web service.
         Response response = given().
-                                accept(ContentType.JSON).
-                            when().
-                                get("/countries").prettyPeek();
+                accept(ContentType.JSON).
+                when().
+                get("/countries").prettyPeek();
+
+        String countryName = response.jsonPath().getString("items.find{it.country_id == 'US'}.country_name");
+        Map<String, Object> countryUS = response.jsonPath().get("items.find{it.country_id == 'US'}");
+        //find all country names from region 2
+        //collectionName.findAll{it.propertyName == 'Value'} -- to get collection objects where property equals to some value
+        //collectionName.find{it.propertyName == 'Value'} -- to object where property equals to some value
+
+        // to get collection properties where property equals to some value
+        //collectionName.findAll{it.propertyName == 'Value'}.propertyName
+        List<String> countryNames = response.jsonPath().getList("items.findAll{it.region_id == 2}.country_name");
+
+        System.out.println("Country name: " + countryName);
+        System.out.println(countryUS);
+        System.out.println(countryNames);
+
+        for (Map.Entry<String, Object> entry : countryUS.entrySet()) {
+            System.out.printf("key = %s, value = %s\n", entry.getKey(), entry.getValue());
+        }
     }
 
 
