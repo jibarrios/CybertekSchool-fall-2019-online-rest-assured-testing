6d09f46f823175cae675f68e79430f15986cf625
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-05-21 20:58:00 -0400

    day 4 API

---
+++ src/test/java/com/automation/tests/day5/POJOPractice.java
@@ -0,0 +1,28 @@
+package com.automation.tests.day5;
+
+import com.automation.utilities.ConfigurationReader;
+import io.restassured.http.ContentType;
+import io.restassured.http.Headers;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.*;
+
+public class POJOPractice {
+
+    @BeforeAll
+    public static void beforeAll(){
+
+    }
+}
