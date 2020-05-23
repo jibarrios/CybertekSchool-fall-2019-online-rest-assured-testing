c49370a5a8a381b8b46588566bf279d72ae000de
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-05-23 17:37:21 -0400

    day 4 API

--- src/test/java/com/automation/tests/day6/POJOPracticeWithPreschool.java
+++ src/test/java/com/automation/tests/day6/POJOPracticeWithPreschool.java
@@ -1,6 +1,35 @@
 package com.automation.tests.day6;
 
+import com.automation.utilities.ConfigurationReader;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.*;
+
 public class POJOPracticeWithPreschool {
 
+    @BeforeAll
+    public static void setup() {
+        baseURI = ConfigurationReader.getProperty("PRESCHOOL.URI");
+    }
 
+    @Test
+    public void addStudentTest() {
+        File file = new File("student.json");
+        Response response = given().
+                contentType(ContentType.JSON).
+                body(file).
+                when().
+                post("/student/create").prettyPeek();
+
+        int studentId = response.jsonPath().getInt("studentId");
+        System.out.println("Student id: " + studentId);
+    }
 }
