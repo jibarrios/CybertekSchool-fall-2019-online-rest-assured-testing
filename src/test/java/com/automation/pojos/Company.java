80197ef2b1aa92436c23bc713ca99d6471899a67
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-05-23 17:56:28 -0400

    day 4 API

--- src/test/java/com/automation/pojos/Company.java
+++ src/test/java/com/automation/pojos/Company.java
@@ -2,23 +2,74 @@ package com.automation.pojos;
 
 /**
  * "company": {
- *  *         "companyId": 11553,
- *  *         "companyName": "Cybertek",
- *  *         "title": "SDET",
- *  *         "startDate": "02/02/2020",
- *  *         "address": {
- *  *             "addressId": 11573,
- *  *             "street": "7925 Jones Branch Dr",
- *  *             "city": "McLean",
- *  *             "state": "Virginia",
- *  *             "zipCode": 22102
- *  *         }
- *  *     }
+ * *         "companyId": 11553,
+ * *         "companyName": "Cybertek",
+ * *         "title": "SDET",
+ * *         "startDate": "02/02/2020",
+ * *         "address": {
+ * *             "addressId": 11573,
+ * *             "street": "7925 Jones Branch Dr",
+ * *             "city": "McLean",
+ * *             "state": "Virginia",
+ * *             "zipCode": 22102
+ * *         }
+ * *     }
  */
 public class Company {
     private int companyId;
     private String companyName;
     private String title;
     private String startDate;
+    private Address address;
 
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(int companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "companyId=" + companyId +
+                ", companyName='" + companyName + '\'' +
+                ", title='" + title + '\'' +
+                ", startDate='" + startDate + '\'' +
+                ", address=" + address +
+                '}';
+    }
 }
--- src/test/java/com/automation/pojos/Student.java
+++ src/test/java/com/automation/pojos/Student.java
@@ -2,38 +2,38 @@ package com.automation.pojos;
 
 /**
  * {
- *     "studentId": 11613,
- *     "firstName": "James",
- *     "lastName": "Bond",
- *     "batch": 15,
- *     "joinDate": "01/01/3321",
- *     "birthDate": "01/01/1890",
- *     "password": "1234",
- *     "subject": "Math",
- *     "gender": "Males",
- *     "admissionNo": "1234",
- *     "major": "CS",
- *     "section": "101010",
- *     "contacts": {
- *         "contactId": 11633,
- *         "phone": "240-123-1231",
- *         "emailAddress": "james_bond@gmail.com",
- *         "premanentAddress": "7925 Jones Branch Dr"
- *     },
- *     "company": {
- *         "companyId": 11553,
- *         "companyName": "Cybertek",
- *         "title": "SDET",
- *         "startDate": "02/02/2020",
- *         "address": {
- *             "addressId": 11573,
- *             "street": "7925 Jones Branch Dr",
- *             "city": "McLean",
- *             "state": "Virginia",
- *             "zipCode": 22102
- *         }
- *     }
+ * "studentId": 10662,
+ * "firstName": "Vera",
+ * "lastName": "Jakson",
+ * "batch": 14,
+ * "joinDate": "02/02/2020",
+ * "birthDate": "02/02/1985",
+ * "password": "123",
+ * "subject": "string",
+ * "gender": "F",
+ * "admissionNo": "string",
+ * "major": "abc",
+ * "section": "12",
+ * "contact": {
+ * "contactId": 10662,
+ * "phone": "7738557985",
+ * "emailAddress": "aaa@gmail.com",
+ * "premanentAddress": "123 main str"
+ * },
+ * "company": {
+ * "companyId": 10662,
+ * "companyName": "Cybertek",
+ * "title": "string",
+ * "startDate": "string",
+ * "address": {
+ * "addressId": 10662,
+ * "street": " 100 S Clark str",
+ * "city": "Chicago",
+ * "state": "IL",
+ * "zipCode": 60606
  * }
+ * }
+ * },
  */
 public class Student {
     private int studentId;
@@ -49,6 +49,133 @@ public class Student {
     private String major;
     private String section;
     private Contact contact;
+    private Company company;
 
+    public int getStudentId() {
+        return studentId;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getBatch() {
+        return batch;
+    }
+
+    public void setBatch(int batch) {
+        this.batch = batch;
+    }
+
+    public String getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(String joinDate) {
+        this.joinDate = joinDate;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAdmissionNo() {
+        return admissionNo;
+    }
+
+    public void setAdmissionNo(String admissionNo) {
+        this.admissionNo = admissionNo;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId=" + studentId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", batch=" + batch +
+                ", joinDate='" + joinDate + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                ", password='" + password + '\'' +
+                ", subject='" + subject + '\'' +
+                ", gender='" + gender + '\'' +
+                ", admissionNo='" + admissionNo + '\'' +
+                ", major='" + major + '\'' +
+                ", section='" + section + '\'' +
+                ", contact=" + contact +
+                ", company=" + company +
+                '}';
+    }
 }
