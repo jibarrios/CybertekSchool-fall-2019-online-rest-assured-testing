6349ec2b858b504183bec90b9eed88cf925c97ae
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-05-28 21:48:42 -0400

    day 7 Authentication and Authorization

--- src/test/java/com/automation/tests/day9/SchemaValidation.java
+++ src/test/java/com/automation/tests/day9/SchemaValidation.java
@@ -1,8 +1,11 @@
 package com.automation.tests.day9;
 
 import com.automation.utilities.ConfigurationReader;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
 
 import static io.restassured.RestAssured.*;
 
@@ -17,6 +20,12 @@ public class SchemaValidation {
 
     @Test
     public void schemaValidationTest(){
+        //get json schema and store it in file object
+        File schema = new File("spartan_schema.json");
 
+        get("/spartans/{id}", 35).
+                prettyPeek().
+        then().
+                body(JsonSchemaValidator.matchesJsonSchema(schema));
     }
 }
